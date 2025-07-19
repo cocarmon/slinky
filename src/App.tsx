73a8b7b724a4b63eec8ed98ba7f872c609dbacad
Cody Carmony <codywcarmony@gmail.com>
2025-07-19 13:36:11 -0400

    seperate start into local state for clear seperation, memoize maze so you can reuse it between the two engines

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Canvas from "./features/canvas/Canvas"
 import Sidebar from "@/components/layout/Sidebar"
 import { useMazeContext } from "./context/MazeContext";
+import { generateMaze } from "./utils/mazeGenerator";
 
 function App() {
+    const { options } = useMazeContext();
     const [resetKey, setResetKey] = useState(0);
-    const { options,setOptions } = useMazeContext();
+    const [isStart, setIsStart] = useState(false);
+    const maze = useMemo(()=>generateMaze(options.size),[options.size]);
 
+    const handleOnStart = useCallback(() => {
+      setIsStart(p => !p);
+    },[]);
 
-    const handleStartChange = () => {
-      setOptions((prev)=> ({
-          ...prev,
-          start: !prev.start
-      }));
-    };
-
-    const handleOnReset = () => {
+    const handleOnReset = useCallback(() => {
       setResetKey(p => p + 1);
-    };
+    }, []);
+
 
   return (
     <div className="flex h-screen">
-      <Sidebar handleOnStart={handleStartChange} handleOnReset={handleOnReset}/>
-      <Canvas key={resetKey} size={Number(options.size)} start={options.start} resetStart={handleStartChange}/>
+      <Sidebar handleOnStart={handleOnStart} handleOnReset={handleOnReset}/>
+      <Canvas key={resetKey} size={options.size} name={options.algorithm} grid={maze} start={isStart} resetStart={handleOnStart}/>
     </div>
   )
 }
